f26cf483ea6feb844647c4d0da95d9dddc013d27
CaedenZ <zfy0120@gmail.com>
2022-10-12 17:04:12 +0800

    nftworks

--- src/web3/testContract.js
+++ src/web3/testContract.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Web3 = require('web3');
+const Tx = require('ethereumjs-tx')
 
 // Connect to local Ethereum node
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -46,7 +47,7 @@ var address = '0xf17f52151EbEF6C7334FAD080c5704D77216b732'
 /* }) */
 
 
-const krisToken = new web3.eth.Contract(abi,'0xFa5B6432308d45B54A1CE1373513Fab77166436f')
+const krisToken = new web3.eth.Contract(abi,'0x2C2B9C9a4a25e24B174f26114e8926a9f2128FE4')
 /* krisToken.methods.getOwner().call(function (err, res) { */
 /*   if (err) { */
 /*     console.log("An error occured", err) */
@@ -57,15 +58,50 @@ const krisToken = new web3.eth.Contract(abi,'0xFa5B6432308d45B54A1CE1373513Fab77
 
 const tokenUri = "https://gateway.pinata.cloud/ipfs/QmYueiuRNmL4MiA2GwtVMm6ZagknXnSpQnB3z2gWbz36hP"
 
+const owner = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57'
+const ownerKey = 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'
+
+
+
+
 // Call mintNFT function
-const mintNFT = () => {
-    krisToken.methods.safeMint(address, tokenUri)
-    console.log(`NFT Minted!`)
+const mintNFT = async () => {
+  const networkId = await web3.eth.net.getId()
+
+  const tx = krisToken.methods.safeMint(owner,tokenUri)
+
+  const gas = await tx.estimateGas({from:owner})
+
+  const gasPrice = await web3.eth.getGasPrice()
+  const data = tx.encodeABI()
+  const nonce = await web3.eth.getTransactionCount(owner)
+  
+  const signedTx = await web3.eth.accounts.signTransaction({
+    to:krisToken.options.address,
+    data,
+    gas,
+    gasPrice,
+    nonce,
+    chainId:networkId
+  },
+  ownerKey)
+  
+  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+  console.log(`transaction hash: ${receipt.transactionHash}`)
+  /*   krisToken.methods.safeMint(address, tokenUri).estimateGas({from:owner}).then(function(gasAmount){ */
+  /*   console.log(gasAmount) */
+  /*   krisToken.methods.safeMint(address,tokenUri).send({from:owner,gas:gasAmount}).then(function(){ */
+  /*     console.log('NFT Minted') */
+  /*   }) */
+  /* }) */
+
+    /* krisToken.methods.safeMint(address, tokenUri).send({from:address,gas:5000000}) */
+    /* console.log(`NFT Minted!`) */
 }
 
 
 const balanceOf = () => {
-    krisToken.methods.balanceOf(address).call(function (err, res) {
+    krisToken.methods.balanceOf(owner).call(function (err, res) {
       if (err) {
         console.log("An error occured", err)
         return
